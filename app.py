902d7a7a5c6f238853b2839b6bb673679b9697b3
Thrinadh-Kumpatla <kthrinadh8898@gmail.com>
2025-01-21 18:55:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Docker boss! you're great bosses blames me always and always and always"
+    return "Hello, Docker boss! you're great bosses blames me always and always and always and boom"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
