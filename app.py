90472e744ea638b4ae73750c1f86718773c8211c
Thrinadh-Kumpatla <kthrinadh8898@gmail.com>
2025-01-29 03:56:47 +0530

    fix signing final

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Docker boss! you're great bosses blames me always and always ultra maxx and core and make it final"
+    return "Hello, Docker boss! you're great bosses blames me always and always ultra maxx and core and make it final call"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
