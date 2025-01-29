778443b676ec5e90d7c41e82b632a3c22f3d7e8b
Thrinadh-Kumpatla <kthrinadh8898@gmail.com>
2025-01-30 01:01:24 +0530

    changes-13

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "export COSIGN_REPOSITORY=hub.dev-rudder.rudderlabs.com/dockerhub-proxy"
+    return "Hello image"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
